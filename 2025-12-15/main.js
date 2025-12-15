806461d411b6d6684a86a57523d9a9d528c606ba
김성근 <78168345+skkim123@users.noreply.github.com>
2025-12-15 23:36:43 +0900

    2025-12-15 2

--- 2025-12-15/main.js
+++ 2025-12-15/main.js
@@ -1,4 +1,15 @@
 // npx tsc main.ts && node main.js
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 /* 문제 1 : 다음 변수들의 타입을 지정해주세요 */
 var userName; // 예: 이름
 var userAge; // 예: 나이
@@ -74,3 +85,66 @@ console.log(isPrimitive(null)); // true
 console.log(isPrimitive(undefined)); // true
 console.log(isPrimitive({})); // false
 console.log(isPrimitive([])); // false
+var user = {
+    name: "Alice",
+    isAdmin: true,
+};
+user = {
+    name: "Bob",
+    age: 40,
+    isAdmin: false,
+};
+/* 문제2. 읽기 전용(readonly) 배열을 생성하고, 배열에 직접 값을 추가하거나 변경하려고 하면 오류가 발생해야 합니다. */
+// 숫자만 담을 수 있는 읽기 전용 배열을 작성하세요.
+// 아래 코드는 오류가 발생해야 합니다.
+var numbers = [];
+// numbers.push(4);
+// numbers[0] = 42;
+// 문제3. 주어진 문제 1,2 번을 푸시오
+// 1. 상품 이름과 가격만을 포함하는 새로운 배열을 생성하는 함수를 작성하세요.
+// 2. 재고가 있는 상품만 포함하는 배열을 반환하는 함수를 작성하세요.
+var products = [
+    ["Laptop", 1000, true],
+    ["Shoes", 50, false],
+    ["Book", 20, true],
+];
+// 1. 상품 이름과 가격만 반환,리턴타입 정의필요
+function getProductNamesAndPrices(products) {
+    // 여기에 구현
+    return products.map(function (v) { return [v[0], v[1]]; });
+}
+// 2. 재고가 있는 상품만 반환,리턴타입 정의필요
+function getAvailableProducts(products) {
+    // 여기에 구현
+    return products.filter(function (v) { return v[2] === true; });
+}
+// 테스트 코드
+console.log(getProductNamesAndPrices(products));
+// 기대 출력: [["Laptop", 1000], ["Shoes", 50], ["Book", 20]]
+console.log(getAvailableProducts(products));
+// 기대 출력: [["Laptop", 1000, true], ["Book", 20, true]]
+// 문제 4. 사용자 정보를 업데이트하는 함수를 작성하세요. 나이가 제공되지 않으면 기본값으로 18을 사용하세요
+//매개변수, 리턴 타입 정의 필요
+function updateUser(user) {
+    return __assign(__assign({}, user), { age: user.age || 18 });
+}
+// 테스트 코드
+console.log(updateUser({ name: "Charlie" })); // { name: "Charlie", age: 18 }
+console.log(updateUser({ name: "Dana", age: 25 })); // { name: "Dana", age: 25 }
+var _products = [
+    { name: "Laptop", price: 1000, category: "Electronics" },
+    { name: "Shoes", price: 50, category: "Fashion" },
+    { name: "Book", price: 20 },
+];
+//매개변수, 리턴 타입 정의 필요
+function getProductsByCategory(category) {
+    // 여기에 구현
+    // 배열 함수에는 타입스크립트 타입 추론이 똑똑하게 안 들어가서, 찾아보니 타입 가드라는 것이 있어서 사용했습니다.
+    return _products
+        .filter(function (product) { return product.category === category; })
+        .map(function (product) { return product.name; });
+}
+// 테스트 코드
+console.log(getProductsByCategory("Electronics")); // ["Laptop"]
+console.log(getProductsByCategory("Fashion")); // ["Shoes"]
+console.log(getProductsByCategory("Books")); // []
--- 2025-12-15/main.ts
+++ 2025-12-15/main.ts
@@ -89,3 +89,103 @@ console.log(isPrimitive(null)); // true
 console.log(isPrimitive(undefined)); // true
 console.log(isPrimitive({})); // false
 console.log(isPrimitive([])); // false
+
+/* 문제 1.아래 객체를 보고 user의 타입을 작성하세요 */
+type User = {
+  name: string;
+  age?: number;
+  isAdmin: boolean;
+};
+
+let user: User = {
+  name: "Alice",
+  isAdmin: true,
+};
+
+user = {
+  name: "Bob",
+  age: 40,
+  isAdmin: false,
+};
+
+/* 문제2. 읽기 전용(readonly) 배열을 생성하고, 배열에 직접 값을 추가하거나 변경하려고 하면 오류가 발생해야 합니다. */
+// 숫자만 담을 수 있는 읽기 전용 배열을 작성하세요.
+
+// 아래 코드는 오류가 발생해야 합니다.
+const numbers: readonly number[] = [];
+// numbers.push(4);
+// numbers[0] = 42;
+
+// 문제3. 주어진 문제 1,2 번을 푸시오
+// 1. 상품 이름과 가격만을 포함하는 새로운 배열을 생성하는 함수를 작성하세요.
+// 2. 재고가 있는 상품만 포함하는 배열을 반환하는 함수를 작성하세요.
+const products: [string, number, boolean][] = [
+  ["Laptop", 1000, true],
+  ["Shoes", 50, false],
+  ["Book", 20, true],
+];
+
+// 1. 상품 이름과 가격만 반환,리턴타입 정의필요
+function getProductNamesAndPrices(
+  products: [string, number, boolean][]
+): [string, number][] {
+  // 여기에 구현
+  return products.map((v) => [v[0], v[1]]);
+}
+
+// 2. 재고가 있는 상품만 반환,리턴타입 정의필요
+function getAvailableProducts(
+  products: [string, number, boolean][]
+): [string, number, boolean][] {
+  // 여기에 구현
+  return products.filter((v) => v[2] === true);
+}
+
+// 테스트 코드
+console.log(getProductNamesAndPrices(products));
+// 기대 출력: [["Laptop", 1000], ["Shoes", 50], ["Book", 20]]
+
+console.log(getAvailableProducts(products));
+// 기대 출력: [["Laptop", 1000, true], ["Book", 20, true]]
+
+// 문제 4. 사용자 정보를 업데이트하는 함수를 작성하세요. 나이가 제공되지 않으면 기본값으로 18을 사용하세요
+//매개변수, 리턴 타입 정의 필요
+function updateUser(user: { name: string; age?: number }): {
+  name: string;
+  age: number;
+} {
+  return {
+    ...user,
+    age: user.age || 18,
+  };
+}
+
+// 테스트 코드
+console.log(updateUser({ name: "Charlie" })); // { name: "Charlie", age: 18 }
+console.log(updateUser({ name: "Dana", age: 25 })); // { name: "Dana", age: 25 }
+
+// 문제5. 아래와 같은 데이터 구조를 사용하여 특정 카테고리에 해당하는 상품의 이름을 출력하는 함수를 작성하세요.
+// proudcts 타입정의 필요
+type Product = { name: string; price: number; category?: string };
+type ProductWithCategory = { name: string; price: number; category: string };
+const _products: Product[] = [
+  { name: "Laptop", price: 1000, category: "Electronics" },
+  { name: "Shoes", price: 50, category: "Fashion" },
+  { name: "Book", price: 20 },
+];
+
+//매개변수, 리턴 타입 정의 필요
+function getProductsByCategory(category: string): string[] {
+  // 여기에 구현
+  // 배열 함수에는 타입스크립트 타입 추론이 똑똑하게 안 들어가서, 찾아보니 타입 가드라는 것이 있어서 사용했습니다.
+  return _products
+    .filter(
+      (product): product is ProductWithCategory => product.category === category
+    )
+    .map((product) => product.name);
+}
+
+// 테스트 코드
+console.log(getProductsByCategory("Electronics")); // ["Laptop"]
+console.log(getProductsByCategory("Fashion")); // ["Shoes"]
+console.log(getProductsByCategory("Books")); // []
